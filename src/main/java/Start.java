70ad28f168931119f699d92bdbe9f089e4877cc9
Davide Michielin <davidemichie@gmail.com>
2021-01-25 22:56:08 +0100

    we can now open the reversi game wikipedia for the rules clicking on ?

--- src/main/java/Start.java
+++ src/main/java/Start.java
@@ -1,129 +1,105 @@
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
 import java.awt.event.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
-public class Start extends JFrame implements ActionListener{
+public class Start implements ActionListener {
+    private int mouseX, mouseY;
+    private final JFrame frame;
 
     public Start() {
+        frame = new JFrame();
 
+        JPanel topPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
 
-        final JPanel topPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
-        final JButton help = new JButton("?");
-        final JButton exit = new JButton("X");
+        topPanel.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseDragged(MouseEvent e) {
+                frame.setLocation(frame.getX() + e.getX() - mouseX, frame.getY() + e.getY() - mouseY);
+            }
+        });
+        topPanel.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                mouseX = e.getX();
+                mouseY = e.getY();
+            }
+        });
+        frame.add(topPanel, BorderLayout.NORTH);
+        topPanel.setBackground(Color.LIGHT_GRAY);
+        final JLabel help = new JLabel("?");
+        final JLabel exit = new JLabel("X");
+        exit.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                System.exit(0);
+            }
+        });
+        help.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                try {
+                    String url = "https://en.wikipedia.org/wiki/Reversi#Rules";
+
+                    Desktop dt = Desktop.getDesktop();
+                    URI uri = new URI(url);
+                    dt.browse(uri.resolve(uri));
+                } catch (URISyntaxException ex) {
+                    ex.printStackTrace();
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+
+            }
+        });
+        exit.setBorder(new EmptyBorder(0, 10, 0, 10));
+        exit.setSize(10, 5);
+        exit.setFont(new Font("Tahoma", Font.BOLD, 15));
+        help.setBorder(new EmptyBorder(0, 10, 0, 10));
+        help.setSize(10, 5);
+        help.setFont(new Font("Tahoma", Font.BOLD, 15));
         topPanel.add(help);
         topPanel.add(exit);
-        add(topPanel,BorderLayout.NORTH);
 
-        help.addActionListener(this);
-        exit.addActionListener(this);
 
         JPanel container = new JPanel(new GridBagLayout());
-        add(container, BorderLayout.CENTER);
-        JPanel buttonMenu = new JPanel(new GridLayout(1,3,5,10));
-        JButton oneVsOne = new JButton("vs 2Player");
+        frame.add(container, BorderLayout.CENTER);
+        JPanel buttonMenu = new JPanel(new GridLayout(1, 3, 5, 10));
+        JButton oneVsOne = new JButton("2 Player Local");
         JButton oneVsCPU = new JButton("vs Computer");
-        JButton online = new JButton("Play");
+        JButton online = new JButton("Online");
         buttonMenu.add(oneVsOne);
         buttonMenu.add(oneVsCPU);
         buttonMenu.add(online);
         container.add(buttonMenu);
         online.addActionListener(this);
-        setLocationRelativeTo(null); //center of the screen
-        setSize(500,300);
-        setUndecorated(true);
-        setResizable(false);
-        setTitle("Othello-Reversi Menu");
-        setVisible(true);
-
-        //JPanel menuPannel = new JPanel(new GridLayout(1, 2));
-
-        //add(menuPannel, BorderLayout.EAST);
-        //JMenuBar menu = new JMenuBar();
-        //menuPannel.add(menu);
-        //JMenu help = new JMenu("Help");
-        //JMenuItem rules = new JMenuItem("Rule");
-        //JMenuItem x = new JMenuItem("X");
-        //menu.add(help);
-        //help.add(rules);
-        //menu.add(rules);
-        //menu.add(x);
-
-
-
-
-
-        /*SpinnerModel value =
-                new SpinnerNumberModel(8,
-                        4,
-                        26,
-                        2);
-
-        String[] typeOfGame = {"Othello", "Reversi"};
-
-        JFrame frame = new JFrame("Othello/Reversi");
-        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        JPanel contenitore = new JPanel(new GridBagLayout());
-        GridBagConstraints c = new GridBagConstraints();
-        c.gridx = 0;
-        c.gridy = 0;
-        ++c.gridy;
-        ++c.gridy;
-        ++c.gridy;
-
-        JPanel menu = new JPanel(new GridLayout(1,1 ));
-        menu.setPreferredSize(new Dimension(500,35));
-        menu.add(new JLabel("Menu Game",SwingConstants.CENTER));
-        contenitore.add(menu,c);
-
-        JPanel playerSelection = new JPanel(new GridLayout(2, 3));
-        playerSelection.setPreferredSize(new Dimension(400, 50));
-        playerSelection.add(new JLabel("Player-1:", SwingConstants.CENTER));
-        playerSelection.add(getEmptyLabel(new Dimension(50, 25)));
-        playerSelection.add(new JLabel("Player-2:", SwingConstants.CENTER));
-        playerSelection.add(new JTextField());
-        playerSelection.add(getEmptyLabel(new Dimension(50, 25)));
-        playerSelection.add(new JTextField());
-        ++c.gridy;
-        contenitore.add(playerSelection,c);
-
-        JPanel rowsAndColumnSelection = new JPanel(new GridLayout(1, 2, 10, 3  ));
-        rowsAndColumnSelection.setPreferredSize(new Dimension(400, 30));
-        rowsAndColumnSelection.add(new JLabel("Rows and Column", SwingConstants.CENTER));
-
-        rowsAndColumnSelection.add(new JSpinner(value));
-        ++c.gridy;
-        contenitore.add(rowsAndColumnSelection,c);
-
-        JPanel selectTypeOfGame = new JPanel(new GridLayout(2,1));
-        selectTypeOfGame.setPreferredSize(new Dimension(200,35));
-        selectTypeOfGame.add(new JLabel("Type Of Game", SwingConstants.CENTER));
-        selectTypeOfGame.add(new JComboBox(typeOfGame));
-        ++c.gridy;
-        contenitore.add(selectTypeOfGame,c);
-
-        JPanel submissionPanel = new JPanel(new GridLayout(1, 1));
-        JButton startGame = new JButton("Start Game");
-        submissionPanel.add(startGame);
-        ++c.gridy;
-        contenitore.add(submissionPanel,c);
-
-        frame.setContentPane(contenitore);
-        frame.pack();
-        frame.setLocationRelativeTo(null);
+        frame.setLocationRelativeTo(null); //center of the screen
+        frame.setSize(500, 300);
+        frame.setUndecorated(true);
         frame.setResizable(false);
+        frame.setTitle("Othello-Reversi Menu");
         frame.setVisible(true);
-
-        frame.setVisible(true);*/
     }
 
-    public void actionPerformed(ActionEvent e){
-        switch(e.getActionCommand()){
-            case "Play":
-                new Play();
-                setVisible(false);
+    public void actionPerformed(ActionEvent e) {
+        switch (e.getActionCommand()) {
+            case "2 Player Local":
+                new OneVsOne();
+                frame.setVisible(false);
+                break;
+            case "vs Computer":
+                new OneVsCPU();
+                frame.setVisible(false);
+                break;
+            case "Onlinr":
+                new Online();
+                frame.setVisible(false);
                 break;
             case "?":
-                JOptionPane.showMessageDialog(this, "Descrizione del gioco");
+                JOptionPane.showMessageDialog(frame, "Descrizione del gioco");
                 break;
             case "X":
                 System.exit(0);
