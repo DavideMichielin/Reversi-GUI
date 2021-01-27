059eebcf593b67d03faf761b95c971c4a74b228f
Adriano Tumino <adrianotumino@gmail.com>
2021-01-27 12:41:35 +0100

    Add instructions for other class

--- src/main/java/Online.java
+++ src/main/java/Online.java
@@ -1,2 +1,11 @@
-public class Online {
+import javax.swing.*;
+import java.awt.event.ActionListener;
+
+public class Online extends JFrame{
+
+    //Radius Host - Client
+
+    //Se Host mostra label con il proprio IP
+
+    //Se client mostra JTextField dove inserire l'ip
 }
