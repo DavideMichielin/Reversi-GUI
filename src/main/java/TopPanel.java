e2f2cf3985d093ec110aaf09d09f8d539cac664a
Adriano Tumino <adrianotumino@gmail.com>
2021-01-27 12:34:35 +0100

    Now TopPanel implements MouseListener

--- src/main/java/TopPanel.java
+++ src/main/java/TopPanel.java
@@ -3,54 +3,23 @@ import javax.swing.border.EmptyBorder;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 
-public class TopPanel {
+public class TopPanel implements MouseListener{
     private final JPanel topPanel;
+    private final JFrame frame;
+
     public TopPanel(){
+        this.frame = null;
         topPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
         topPanel.setBackground(Color.LIGHT_GRAY);
         final JLabel help = new JLabel("?");
         final JLabel exit = new JLabel("X");
-        exit.addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseClicked(MouseEvent e) {
-                System.exit(0);
-            }
-
-            @Override
-            public void mouseEntered(MouseEvent e) {
-                exit.setOpaque(true);
-                exit.setBackground(Color.RED);
-                exit.setCursor(new Cursor(Cursor.HAND_CURSOR));
-            }
-            @Override
-            public void mouseExited(MouseEvent e) {
-                exit.setBackground(Color.LIGHT_GRAY);
-            }
-        });
-        help.addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseClicked(MouseEvent e) {
-                try {
-                    String url = "https://en.wikipedia.org/wiki/Reversi#Rules";
-                    Desktop dt = Desktop.getDesktop();
-                    URI uri = new URI(url);
-                    dt.browse(uri.resolve(uri));
-                } catch (URISyntaxException | IOException ex) {
-                    ex.printStackTrace();
-                }
-            }
-
-            @Override
-            public void mouseEntered(MouseEvent e) {
-                help.setCursor(new Cursor(Cursor.HAND_CURSOR));
-            }
-
-
-        });
+        exit.addMouseListener(this);
+        help.addMouseListener(this);
         exit.setBorder(new EmptyBorder(0, 10, 0, 10));
         exit.setSize(10, 5);
         exit.setFont(new Font("Tahoma", Font.BOLD, 15));
@@ -62,6 +31,7 @@ public class TopPanel {
     }
 
     public TopPanel( final JFrame frame){
+        this.frame = frame;
         topPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
         topPanel.setBackground(Color.LIGHT_GRAY);
         JPanel topPanelRight = new JPanel(new FlowLayout(FlowLayout.RIGHT));
@@ -79,53 +49,9 @@ public class TopPanel {
         final JLabel back = new JLabel("\u2190 Back");
         final JLabel help = new JLabel("?");
         final JLabel exit = new JLabel("X");
-        exit.addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseClicked(MouseEvent e) {
-                System.exit(0);
-            }
-
-            @Override
-            public void mouseEntered(MouseEvent e) {
-                exit.setOpaque(true);
-                exit.setBackground(Color.RED);
-                exit.setCursor(new Cursor(Cursor.HAND_CURSOR));
-            }
-            @Override
-            public void mouseExited(MouseEvent e) {
-                exit.setBackground(Color.LIGHT_GRAY);
-            }
-        });
-        help.addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseClicked(MouseEvent e) {
-                try {
-                    String url = "https://en.wikipedia.org/wiki/Reversi#Rules";
-                    Desktop dt = Desktop.getDesktop();
-                    URI uri = new URI(url);
-                    dt.browse(uri.resolve(uri));
-                } catch (URISyntaxException | IOException ex) {
-                    ex.printStackTrace();
-                }
-            }
-
-            @Override
-            public void mouseEntered(MouseEvent e) {
-                help.setCursor(new Cursor(Cursor.HAND_CURSOR));
-            }
-        });
-        back.addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseClicked(MouseEvent e) {
-                new Start();
-                frame.setVisible(false);
-            }
-
-            @Override
-            public void mouseEntered(MouseEvent e) {
-                back.setCursor(new Cursor(Cursor.HAND_CURSOR));
-            }
-        });
+        exit.addMouseListener(this);
+        help.addMouseListener(this);
+        back.addMouseListener(this);
         exit.setBorder(new EmptyBorder(0, 10, 0, 10));
         exit.setSize(10, 5);
         exit.setFont(new Font("Tahoma", Font.BOLD, 15));
@@ -141,5 +67,61 @@ public class TopPanel {
 
     public JPanel getTopPanel() {
         return topPanel;
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        JLabel clickedLabel = (JLabel) e.getSource();
+        switch (clickedLabel.getText()){
+            case "X":
+                System.exit(0);
+                break;
+            case "?":
+                try {
+                    String url = "https://en.wikipedia.org/wiki/Reversi#Rules";
+                    Desktop dt = Desktop.getDesktop();
+                    URI uri = new URI(url);
+                    dt.browse(uri.resolve(uri));
+                } catch (URISyntaxException | IOException ex) {
+                    ex.printStackTrace();
+                }
+                break;
+            case "\u2190 Back":
+                new Start();
+                frame.setVisible(false);
+                break;
+            default:
+                break;
+
+        }
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        JLabel overLabel = (JLabel) e.getSource();
+        overLabel.setCursor(new Cursor(Cursor.HAND_CURSOR));
+        if(overLabel.getText().equals("X")){
+            overLabel.setOpaque(true);
+            overLabel.setBackground(Color.RED);
+        }
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+        JLabel overLabel = (JLabel) e.getSource();
+        if(overLabel.getText().equals("X")){
+            overLabel.setOpaque(true);
+            overLabel.setBackground(Color.LIGHT_GRAY);
+        }
     }
 }
