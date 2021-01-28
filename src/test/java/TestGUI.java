7d827982c64e4aed1b58c2d9d70ec0b37ac0dc2d
Adriano Tumino <adrianotumino@gmail.com>
2021-01-28 11:35:07 +0100

    we can check first invalid position

--- src/test/java/TestGUI.java
+++ src/test/java/TestGUI.java
@@ -1,13 +1,21 @@
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
-import static org.junit.Assert.assertFalse;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class TestGUI {
 
     @Test
-    // sto test non mi convince troppo, dovremo vedere se riusciamo a introdurre le cose fatte da Giorgia e Dodo
     public void cell1AIsEmptyAtBoardOfDimension8x8Initialization() {
         GUIGameManager game = new GUIGameManager("Bob", "Alice", 8, "Othello");
         assertFalse(game.isSetDisk(0,0));
     }
+    @ParameterizedTest
+    @CsvSource({"8,0,0", "26,1,1", "4, 0, 0"})
+    public void cellIsEmptyAtBoardOfDimension(int DimensionBoard, int indexR, int indexC){
+        GUIGameManager game = new GUIGameManager("Bob", "Alice", DimensionBoard, "Othello");
+        assertFalse(game.isSetDisk(indexC,indexR));
+    }
+
 }
